10bb97dbd8693e336d6d9eec3d78c91cce5509f0
Megan <meganwei@gmail.com>
2019-09-14 15:27:08 -0400

    a-m + space commands

--- convertlettertocommand.py
+++ convertlettertocommand.py
@@ -6,7 +6,7 @@ Created on Sat Sep 14 13:14:38 2019
 @author: isabellequaye
 """
 
-draw_letter = {}
+draw_letter = {" ": , "A": upsidedownL_shell() + right_bar() + straight_bar(), "B": C_shell() + right_bar() + straight_bar(), "C": C_shell(), "D": , "E": C_shell() + right_bar(), "F": upsidedownL_shell() + right_bar(), "G": C_shell() + "D2" + right_bar() + "U2" + "L1" + "R1" + "D2" + left_bar() + "U2", "H": straight_bar() + right_bar() + straight_bar(), "I": straight_bar(), "J": , "K": straight_bar() + "R1" + "U2" + "D2" + "R1" + "D2" + "U2" + left_bar(), "L": L_shell(), "M": ,}
 def straight_bar():
     return "U2D4U2"
 
